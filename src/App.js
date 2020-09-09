0f2cb11d3e282ae67565fc480d049a57384ca3c3
jm90hong <jm90.hong@gmail.com>
2020-09-09 18:55:17 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="grey-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+
+
+const customers=[{
+    'id':1,
+    'image':'https://placeimg.com/64/64/any',
+    'name':'홍정민',
+    'birthday':'900404',
+    'gender':'남',
+    'job':'대학생'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.com/64/64/any',
+    'name':'홍지원',
+    'birthday':'930404',
+    'gender':'여',
+    'job':'대학생'
+  }
+]
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c =>{
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+      
+    );
+  }
 }
+
+
 
 export default App;
